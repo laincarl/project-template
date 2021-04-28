7819a905a248289b1db487b755f62c7c86928e3b
laincarl <1047972853@qq.com>
2021-04-28 17:29:30 +0800

    feat(global): 添加react热更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import './index.less';
+
 const PageIndex = () => (
   <div>
     PageIndex
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackBar = require('webpackbar');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isEnvProduction = process.env.NODE_ENV === 'production';
+const isEnvDevelopment = process.env.NODE_ENV === 'development';
 const appSrc = path.resolve(__dirname, 'src');
 const appHtml = path.resolve(__dirname, 'public/index.html');
 module.exports = {
-  mode: isEnvProduction ? 'production' : 'development',
+  mode: isEnvDevelopment ? 'development' : 'production',
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  devtool: isEnvProduction ? false : 'cheap-module-source-map',
+  devtool: isEnvDevelopment ? 'cheap-module-source-map' : false,
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
+  target: 'web',
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         include: appSrc,
         loader: 'babel-loader',
+        options: {
+          plugins: [
+            isEnvDevelopment && require.resolve('react-refresh/babel'),
+          ].filter(Boolean),
+        },
       },
       {
         test: /\.less$/i,
@@ -35,6 +43,7 @@ module.exports = {
     ],
   },
   devServer: {
+    hot: true,
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
@@ -64,6 +73,7 @@ module.exports = {
       },
     ),
     new WebpackBar(),
-    !isEnvProduction && new ForkTsCheckerWebpackPlugin(),
+    isEnvDevelopment && new ForkTsCheckerWebpackPlugin(),
+    isEnvDevelopment && new ReactRefreshWebpackPlugin(),
   ].filter(Boolean),
 };
