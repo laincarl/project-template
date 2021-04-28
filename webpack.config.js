c425f65a60bcc12c7295a38a3fe1b2e417fc4140
laincarl <1047972853@qq.com>
2021-04-28 17:33:05 +0800

    perf(global): output加上contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
   },
   devtool: isEnvDevelopment ? 'cheap-module-source-map' : false,
   resolve: {
